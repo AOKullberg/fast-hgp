4072973715e1e8768177c54cfdc4176da06d3e38
Anton Kullberg <anton.kullberg@liu.se>
2024-01-19 11:57:28 +0100

    py: alternative implementation for triu case

--- fastHGP/utils.py
+++ fastHGP/utils.py
@@ -52,14 +52,27 @@ def B(Gamma, indices, md, p):
     return jax.vmap(jax.vmap(T_gamma, (None, None, 0, None, None), 0),
                     (None, None, None, 0, None), 0)(Gamma, md, indices, indices, p)
 
+def combine_with_rep(a, b, m):
+    # "Fast" implementation of combinations_with_replacement -- can **only** combine vectors of the same size
+    # Unfortunately creates a quite big matrix as a substep, which takes time and storage.
+    n = a.shape[0]
+    R = jnp.repeat(a, jnp.arange(n, 0, -1), axis=0, total_repeat_length=m)
+    S = jnp.tile(b[None, :], (n, 1, 1))[*jnp.triu_indices(n), :]
+    return jnp.vstack([R[None,...], S[None,...]])
+
 # Computes upper triangular part of the information/dual matrix
+@jax.jit
 def B_triu(Gamma, indices, md, p):
     # Creates an array with all possible permutations between indices (since I is symmetric we only need to compute the upper triangular part)
     # Assume a matrix A=[a1; a2; a3], combinations_with_replacement creates all possible combinations of a1, a2, a3, assuming that the combinations
     # are "symmetric". I.e., combinations_with_replacement(A) = [(a1, a1), (a1, a2), (a1, a3), (a2, a2), (a2, a3), (a3, a3)].
     # E.g., (a2, a1) is ignored because (a1, a2) exists.
-    tmp = jnp.array(list(combinations_with_replacement(indices, 2)))
-    i, j = tmp[:, 0], tmp[:, 1] # Extract the two index matrices
+    # Faster implementation
+    m = int(indices.shape[0]/2 * (2*indices[0,0] + (indices.shape[0] - 1)))
+    i, j = combine_with_rep(indices, indices, m).T
+    # Legacy implementation
+    # tmp = jnp.array(list(combinations_with_replacement(indices, 2)))
+    # i, j = tmp[:, 0], tmp[:, 1] # Extract the two index matrices
     return jax.vmap(T_gamma, (None, None, 0, 0, None), 0)(Gamma, md, i, j, p)
 
 @jax.jit
